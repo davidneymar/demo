434bc3189c8d7403f8b2877d1f4c16f600c97af2
davidneymar <47298203+davidneymar@users.noreply.github.com>
2024-06-26 14:48:28 +0530

    Add files via upload
    intial commit gradle

---
+++ gradel/demo/src/main/java/com/example/demo/GradelApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradelApplication.class, args);
+	}
+
+}
